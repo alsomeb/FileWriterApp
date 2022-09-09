60eccca1c02da3181200391619f48d55560b5602
alsomeb <alsomeb@gmail.com>
2022-09-09 13:41:18 +0200

    Clean up, removing some comments that was unnessecery

--- src/User.java
+++ src/User.java
@@ -2,8 +2,6 @@ public class User {
     private final String userName;
     private final String email;
 
-    // private static HashMap<String, String> users = new HashMap<>(); // TODO Public static final -> Skapa Users Class istället
-
     public User(String userName, String email) {
         this.userName = userName;
         this.email = email;
